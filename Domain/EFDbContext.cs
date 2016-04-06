f98e93cef02bc8d0b77cf1c23e92b2ccf7472062
Daria Ivanova2 <daria_ivanova2@epam.com>
2016-04-06 20:11:09 +0300

    NoStyles

---
+++ Domain/EFDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Domain.Entities;
+
+namespace Domain
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext(string connectionString) {
+            Database.Connection.ConnectionString = connectionString;
+        }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
---
+++ UsingEF/EFEventsRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Domain;
+using System.Configuration;
+using Domain.Entities;
+
+
+namespace UsingEF
+{
+    public class EFEventsRepository
+    {
+        private EFDbContext context;
+
+        public EFEventsRepository()
+        {
+            context = new EFDbContext(ConfigurationManager.ConnectionStrings[0].ConnectionString);
+        }
+
+        public IEnumerable<Event> GetEvents() {
+            return context.Events;
+        }
+
+        public Event GetEventById(int id) {
+            return context.Events.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
